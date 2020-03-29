03cc1fe958fdecb3d6e3656e436a52cef098ed1b
Brandon <brandonfox@hotmail.co.uk>
2020-03-29 15:30:39 +0700

    Editor side

---
+++ ace-crdt/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const express = require('express')
+  , app = express()
+  , server = require('http').Server(app)
+  , io = require('socket.io')(server)
+  , port = Number(process.env.PORT) || 5000
+
+app.use(express.static('lib'))
+app.use(express.static('node_modules/ace-builds/src-noconflict'))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+let nextUserId = 1
+
+
+io.on('connection', function (socket) {
+  var userId = nextUserId++;
+
+  console.log('connection - assigning id ' + userId);
+  socket.emit('init', { id: userId })
+
+  socket.on('message', op => { socket.broadcast.emit('message', op) })
+})
+
+
+server.listen(port, function () {
+  console.log('listening on *:' + port);
+})
---
+++ ace-crdt/lib/editor.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const editor = ace.edit('editor')
+var socket
+
+let rga
+if (!rga) {
+  editor.setWrapBehavioursEnabled(false)
+  rga = new RGA.AceEditorRGA('testuser', editor)
+}
+
+
+var createSession = function() {
+  if(socket != null){
+    console.log("Already connected to session")
+    return
+  }
+  var xmlHttp = new XMLHttpRequest();
+  console.log("Creating request to make session")
+  xmlHttp.onreadystatechange = function() {
+    if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      opensocket(xmlHttp.responseText)
+  }
+  xmlHttp.open('GET', 'http://localhost:3000/create', true);
+  xmlHttp.send(null);
+}
+
+function joinSession(path){
+  if(socket != null){
+    socket.disconnect()
+  }
+  socket = io('ws://localhost:3000' + path)
+  console.log("Connecting to session " + path)
+  socket.on('init', ({ id, history }) => {
+    editor.setWrapBehavioursEnabled(false)
+    rga = new RGA.AceEditorRGA(id, editor)
+    rga.subscribe(op => { socket.emit('message', op) })
+  
+    socket.on('message', op => { rga.receive(op) })
+  
+    socket.emit('message', { type: 'historyRequest' })
+  });
+}
+
+function opensocket(url){
+  console.log("Got reply for socket session " + url)
+  socket = io('ws://localhost:3000' + url)
+  socket.on('init', ({ id, history }) => {
+    if (!rga) {
+      editor.setWrapBehavioursEnabled(false)
+      rga = new RGA.AceEditorRGA(id, editor)
+    }
+    rga.subscribe(op => { socket.emit('message', op) })
+  
+    socket.on('message', op => { rga.receive(op) })
+  
+    socket.emit('message', { type: 'historyRequest' })
+  });
+}
+editor.focus()
