5b60474b08486a407819c70502e6a4d6eae865c9
Sorawit Kongnurat <trainer.mapring@gmail.com>
2020-03-29 19:39:25 +0700

    upload and download with azure files

---
+++ azure-files/app.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:8080', // restrict calls to those this address
+    methods: ['GET', 'POST'], // only allow GET, POST requests
+  })
+);
+
+app.use(bodyParser.json());
+
+if (!fs.existsSync(path.join(process.cwd(), 'uploads'))) {
+  fs.mkdirSync(path.join(process.cwd(), 'uploads'));
+}
+
+app.get('/upload', (req, res, next) => {
+  if (req.body.folders) {
+    const folders = req.body.folders.split('/');
+    fs.readdir(
+      path.join(process.cwd(), 'uploads', ...folders),
+      (err, files) => {
+        if (err) {
+          res.status(404).end();
+        }
+        res.send(files);
+      }
+    );
+  } else {
+    fs.readdir(path.join(process.cwd(), 'uploads'), (err, files) => {
+      if (err) {
+        res.status(404).end();
+      }
+      res.send(files);
+    });
+  }
+});
+
+app.post('/upload', (req, res, next) => {
+  if (!req.body.uploadPath.includes('/')) {
+    const filename = req.body.uploadPath;
+    fs.writeFile(
+      path.join(process.cwd(), 'uploads', filename),
+      req.body.data,
+      () => {
+        res.end();
+      }
+    );
+  } else {
+    const uploadPath = req.body.uploadPath.split('/');
+    const filename = uploadPath.pop();
+    const folders = Array.from(uploadPath);
+    if (!fs.existsSync(path.join(process.cwd(), 'uploads', ...folders))) {
+      fs.mkdirSync(path.join(process.cwd(), 'uploads', ...folders), {
+        recursive: true,
+      });
+    }
+    fs.writeFile(
+      path.join(process.cwd(), 'uploads', ...folders, filename),
+      req.body.data,
+      () => {
+        res.end();
+      }
+    );
+  }
+});
+
+app.get('/download', (req, res, next) => {
+  const uploadPath = req.query.uploadPath.split('/');
+  console.log(uploadPath);
+  const pathToFile = path.join(process.cwd(), 'uploads', ...uploadPath);
+  const readStream = fs.createReadStream(pathToFile);
+  // // This will wait until we know the readable stream is actually valid before piping
+  readStream.on('open', () => {
+    // This just pipes the read stream to the response object (which goes to the client)
+    readStream.pipe(res);
+  });
+  // // This catches any errors that happen while creating the readable stream (usually invalid names)
+  readStream.on('error', err => {
+    res.end(err);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('listening on 3000');
+});
