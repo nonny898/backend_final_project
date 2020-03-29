a0bb4723d789677aad052c6cbcc29667aab7dfbc
Sorawit Kongnurat <trainer.mapring@gmail.com>
2020-03-29 16:28:07 +0700

    basic editor view

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -3,12 +3,18 @@ import Router from 'vue-router';
 import Login from '../views/Login';
 import Logout from '../views/Logout';
 import Main from '../views/Main.vue'
+import Editor from '../views/Editor.vue'
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/editor',
+      name: 'Editor',
+      component: Editor
+    },
     {
       path: '/login',
       name: 'Login',
