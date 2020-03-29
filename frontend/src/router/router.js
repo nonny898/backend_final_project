84afd05d426611abb40d3f248f3190663fd465d5
Atichard Chintakanond <atichard.c@outlook.com>
2020-03-29 15:21:51 +0700

    add vuex

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,28 +1,46 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from '../views/Login';
-import Logout from '../views/Logout';
-import Main from '../views/Main.vue'
+import store from '../store/store.js';
+import Login from '../views/Login.vue';
+import Logout from '../views/Logout.vue';
+import Main from '../views/Main.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: Login,
     },
     {
       path: '/logout',
       name: 'Logout',
-      component: Logout
+      component: Logout,
     },
     {
       path: '/',
       name: 'Main',
-      component: Main
-    }
-  ]
+      component: Main,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+  ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next();
+      return;
+    }
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
