3f20cba13e442f6d11345fb359951ae2682ed7f8
Brandon <brandonfox@hotmail.co.uk>
2020-03-30 20:40:34 +0700

    Attempting to get vue to take env variables

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
       rules: [
         {
-          test: /config.*config\.js$/,
+          test: /.*config\.js$/,
           use: [
             {
               loader: 'file-loader',
