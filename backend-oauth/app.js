1ae666cd1ed3afc9f7270fd272c99d0592ef693d
Brandon <brandonfox@hotmail.co.uk>
2020-03-31 03:28:31 +0700

    Added support for www and non www frontend domain

--- backend-oauth/app.js
+++ backend-oauth/app.js
@@ -13,7 +13,7 @@ const profileRoutes = require('./routes/profile-routes');
 const app = express();
 app.use(
   cors({
-    origin: `http://${env.CORS_ALLOW}`, // restrict calls to those this address
+    origin: [`http://${env.CORS_ALLOW}`,`http://www.${env.CORS_ALLOW}`], // restrict calls to those this address
     methods: 'GET', // only allow GET requests
     credentials: true,
   })
