7f29bf0f433652f5fd7efa3dbff9c54b2bbdca2a
Natthawit Janjaroen <natwit442@gmail.com>
2020-03-30 22:26:50 +0700

    new joining connected get changes from the session creator as well. (board not wiped out)

--- ace-crdt/lib/editor.js
+++ ace-crdt/lib/editor.js
@@ -1,54 +1,74 @@
 "use strict";
 
 const editor = ace.edit('editor')
-var socket
+editor.setTheme('ace/theme/monokai');
 
 const serveradr = '192.168.39.37:30113/'
+var session = editor.getSession()
+var doc = session.getDocument();
 
+session.setMode('ace/mode/javascript');
+session.setValue(`var you = 'are awesome;'`);
+
+var socket = null;
+var joined = null;
+
+editor.setWrapBehavioursEnabled(false)
 let rga
-if (!rga) {
-  editor.setWrapBehavioursEnabled(false)
-  rga = new RGA.AceEditorRGA('host', editor)
-}
 
 var createSession = function() {
-  if(socket != null){
+  // save the whole text and paste fast
+  if (socket != null) {
     console.log("Already connected to session")
     return
   }
+
   var xmlHttp = new XMLHttpRequest();
   console.log("Creating request to make session")
   xmlHttp.onreadystatechange = function() {
-    if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
-      opensocket(xmlHttp.responseText)
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) opensocket(xmlHttp.responseText, true)
+
   }
   xmlHttp.open('GET', 'http://' + serveradr + 'create', true);
   xmlHttp.send(null);
 }
 
-function joinSession(path){
-  if(socket != null){
+function joinSession(path) {
+  if (socket != null) {
     socket.removeAllListeners()
     socket.disconnect()
   }
-  opensocket(path)
+  opensocket(path, false)
 }
 
-function opensocket(url){
+function opensocket(url, isCreator) {
   console.log("Opening socket on url " + url)
-  socket = io('ws://' + serveradr ,{query: 'session=' + url})
-  socket.on('init', ({ id, history }) => {
+  socket = io('ws://' + serveradr, {
+    query: 'session=' + url
+  })
+  // This process is async that's why
+  socket.on('init', ({
+    id,
+    history
+  }) => {
     console.log("Got id " + id)
-    editor.setWrapBehavioursEnabled(false)
     rga = new RGA.AceEditorRGA(id, editor)
-    rga.subscribe(op => { socket.emit('message', op) })
-  
-    socket.on('message', op => { 
-      rga.receive(op) 
-      console.log(op)
+
+    rga.subscribe(op => {
+      socket.emit('message', op)
     })
-  
-    socket.emit('message', { type: 'historyRequest' })
+
+    socket.on('message', op => rga.receive(op))
+
+    // Only ask for the history when you joined the session
+    if (!isCreator) socket.emit('message', {
+      type: 'historyRequest'
+    })
+    // you are the creator save all text and commit the change to the history
+    else {
+      const allTexts = doc.getAllLines().join('\n');
+      session.setValue(allTexts)
+    }
     editor.focus()
   });
 }
